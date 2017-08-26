dff4d37289a0546a9299d6f3fd6ecf21c947c940
Don Smith <don@antiantonym.com>
2017-08-27 11:00:16 +1200

    swap out replace with sed utility

--- init.sh
+++ init.sh
@@ -8,7 +8,9 @@ fi
 
 PROJECT_NAME=`basename "$PWD"`
 
-replace "tao.docker" "$PROJECT_NAME.docker" -- nginx.conf
-replace "tao" "$PROJECT_NAME" -- nginx-proxy.conf
-replace "NGINX_PORT=80" "NGINX_PORT=$NGINX_PORT" -- .env
-replace "PROJECT_NAME=tao" "PROJECT_NAME=$PROJECT_NAME" -- .env
+# using @ instead of / because $PWD contains /'s
+sed -i "s@PROJECT_ROOT@$PWD@g" nginx-proxy.conf
+sed -i "s/tao/$PROJECT_NAME/g" nginx-proxy.conf
+sed -i "s/tao.docker/$PROJECT_NAME.docker/g" nginx.conf
+sed -i "s/NGINX_PORT=80/NGINX_PORT=$NGINX_PORT/g" .env
+sed -i "s/PROJECT_NAME=tao/PROJECT_NAME=$PROJECT_NAME/g" .env
