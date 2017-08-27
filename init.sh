7f53f4f8e83bedcd419be2948eb0d3c61f7da19e
Don Smith <don@antiantonym.com>
2017-08-27 19:21:44 +1200

    fix an omission during init

--- init.sh
+++ init.sh
@@ -8,9 +8,9 @@ fi
 
 PROJECT_NAME=`basename "$PWD"`
 
-# using @ instead of / because $PWD contains /'s
-sed -i "s@PROJECT_ROOT@$PWD@g" nginx-proxy.conf
-sed -i "s/tao/$PROJECT_NAME/g" nginx-proxy.conf
-sed -i "s/tao.docker/$PROJECT_NAME.docker/g" nginx.conf
+# Using @ instead of / because $PWD contains /'s
+sed -i "s@PROJECT_ROOT_VALUE@$PWD@g" nginx-proxy.conf
+sed -i "s/PROJECT_NAME_VALUE/$PROJECT_NAME/g" nginx-proxy.conf
+sed -i "s/PROJECT_NAME_VALUE/$PROJECT_NAME/g" nginx.conf
 sed -i "s/NGINX_PORT=80/NGINX_PORT=$NGINX_PORT/g" .env
-sed -i "s/PROJECT_NAME=tao/PROJECT_NAME=$PROJECT_NAME/g" .env
+sed -i "s/PROJECT_NAME_VALUE/$PROJECT_NAME/g" .env
